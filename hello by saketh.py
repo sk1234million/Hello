4bb5eae678e04c4aab1851d2c60803e356ea8abf
sk1234million <49349870+sk1234million@users.noreply.github.com>
2019-04-06 18:42:12 -0700

    Add files via upload
    Hello

---
+++ hello by saketh.py
@@ -0,0 +1,24 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 22:22:05) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> person = input('enter your name: ')
+enter your name: Saketh
+>>> print ('Hello', person,'.How are You?,)
+       
+SyntaxError: EOL while scanning string literal
+>>> print ('Hello", person)
+       
+SyntaxError: EOL while scanning string literal
+>>> 
+>>> 
+>>> 
+>>> person = input('Enter Your Name')
+Enter Your Name
+>>> person = input('Enter Your Name: ')
+Enter Your Name: Saketh
+>>> print('Hello', person )
+Hello Saketh
+>>> person= input('Enter Your Name: ')
+Enter Your Name: Saketh
+>>> print('Hello', person)
+Hello Saketh
+>>> print('Hello', person)
